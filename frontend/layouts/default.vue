534f5ea90e36c24e844682f4e50cc1584f9b5302
Turchanov Denis <turchanovdenis786@gmail.com>
2022-12-10 11:02:21 +0200

    default

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+// This will work in both `<script setup>` and `<script>`
+definePageMeta({
+  layout: "default",
+});
+</script>
